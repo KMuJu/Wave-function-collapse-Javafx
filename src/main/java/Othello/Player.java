d189a29fb42391236a6cb8d1b929d59e30cb4527
Kasper <kasper.mustad.juell@gmail.com>
2023-03-24 15:06:44 +0100

    la til noen spill bools

--- src/main/java/Othello/Player.java
+++ src/main/java/Othello/Player.java
@@ -7,21 +7,37 @@ public class Player implements PlayerInterface{
     Tile[][] grid;
     Moves moves;
     int turn;
+    OthelloGame game;
+    boolean spill = true;
 
-    Player(Tile[][] grid, int turn){
+    Player(Tile[][] grid, OthelloGame game, int turn){
         this.grid = grid;
-        moves = new Moves(grid);
+        moves = new Moves(game.getGrid());
+        this.game = game;
+        this.turn = turn;
+    }
+
+    public int getTurn(){
+        return turn;
+    }
+
+    
+    public boolean getSpill(){
+        return spill;
+    }
+
+    public void setSpill(boolean b){
+        spill = b;
     }
 
     @Override
     public void click(int x, int y) {
-        
+        game.change(x, y, turn);
     }
 
     @Override
     public void spill() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'spill'");
+        
     }
     
 }
