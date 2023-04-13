db000b104d56908c404326163bb55b54072daa7c
Kasper <kasper.mustad.juell@gmail.com>
2023-04-13 11:44:47 +0200

    la til javafx swing

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,9 @@
 open module TDT4100_project {
+    requires javafx.swing;
     requires javafx.base;
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
     requires java.xml;
+    requires java.desktop;
 }
